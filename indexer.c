2681e4c7790124b32bc53ad3bee0fb3687448b9f
Filipe Fenrich <foolemdev@gmail.com>
2018-11-20 22:34:02 -0200

    insertion working and search too

--- indexer.c
+++ indexer.c
@@ -9,11 +9,12 @@
 
 typedef struct trie_node {
   struct trie_node *children[ALPHABET_SIZE];
+  char ch;
   bool is_end_of_word;
   int word_counting;
 } node;
 
-node *get_node() {
+node *get_node(void) {
   node *p_node = NULL;
 
   p_node = (node*) malloc(sizeof(node));
@@ -29,23 +30,42 @@ node *get_node() {
 
 void insert_node(node *root, char *word) {
   unsigned int index;
-  node *p_node = NULL;
-  p_node = (node*) malloc(sizeof(node));
-
+  node *current = root;
   for (unsigned int i = 0; i < strlen(word); i++) {
-    index = CHAR_TO_INDEX(word[i]);
-    if (root->children[index] == NULL) {
-      root->children[index] = p_node;
+    if (current->children[CHAR_TO_INDEX(word[i])] == NULL) {
+      current->children[CHAR_TO_INDEX(word[i])] = get_node();
     }
-    root = root->children[index];
+    current = current->children[CHAR_TO_INDEX(word[i])];
+    current->ch = word[i];
   }
-  root->is_end_of_word = true;
-  root->word_counting++;
+  current->is_end_of_word = true;
+  current->word_counting += 1;
+}
+
+bool search(node *root, char *word) {
+  for (unsigned int i = 0; i < strlen(word); i++) {
+
+    if (root->children[CHAR_TO_INDEX(word[i])] == NULL) {
+      return false;
+    }
+    root = root->children[CHAR_TO_INDEX(word[i])];
+  }
+  return root->is_end_of_word;
 }
 
 int main() {
-  node p = *get_node();
+  node *p = get_node();
   char *ch = "abelha";
-  insert_node(&p, ch);
+  insert_node(p, ch);
+  char *ch1 = "carro";
+  insert_node(p, ch1);
+  char *ch2 = "porta";
+  insert_node(p, ch2);
+  char *ch3 = "porra";
+  insert_node(p, ch3);
+  printf("%d\n", search(p, "abelhaa"));
+  printf("%d\n", search(p, "carro"));
+  printf("%d\n", search(p, "porsa"));
+
   return 0;
 }
